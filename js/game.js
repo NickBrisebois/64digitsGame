e9b178b547b8e28adee2a2caf92429891d1b0cb0
NickBrisebois <nickbrisebois12@gmail.com>
2014-04-13 17:15:48 -0400

    Added two paddles and gave player ability to move

--- js/game.js
+++ js/game.js
@@ -1,13 +1,28 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
-function preload() {
+var player, opponent;
 
-}
+function preload() {
+	game.load.image('paddle', 'Images/paddle.png');
+}	
 
 function create() {
+	player = game.add.sprite(40, 30, 'paddle');
+	opponent = game.add.sprite(750, 30, 'paddle');
 
+	game.physics.enable(player, Phaser.Physics.ARCADE);
+	game.physics.enable(opponent, Phaser.Physics.ARCADE);
+
+	player.body.collideWorldBounds = true;
+	opponent.body.collideWorldBounds = true;
+
+	game.input.keyboard.addKeyCapture( [ Phaser.Keyboard.W, Phaser.Keyboard.S, Phaser.Keyboard.SPACEBAR ] );
 }
 
 function update() {
-
+	if( game.input.keyboard.isDown(Phaser.Keyboard.S) ){
+		player.body.y += 5;
+	}else if( game.input.keyboard.isDown(Phaser.Keyboard.W) ){
+		player.body.y -= 5;
+	}
 }
