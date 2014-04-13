9e0992d773773ed094576b2ab3e54d917746c336
NickBrisebois <nickbrisebois12@gmail.com>
2014-04-13 18:59:35 -0400

    Ball can now be shot like in Angry Birds (sort of). Paddles have weird physics

--- js/game.js
+++ js/game.js
@@ -1,26 +1,45 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
-var player, opponent;
+var player, opponent, ball;
+var gameStarted = false;
 
 function preload() {
 	game.load.image('paddle', 'Images/paddle.png');
+	game.load.image('ball', 'Images/ball.png');
 }	
 
 function create() {
-	player = game.add.sprite(40, 30, 'paddle');
-	opponent = game.add.sprite(750, 30, 'paddle');
+
+	paddles = game.add.group();
+
+	player = paddles.create(40, 30, 'paddle');
+	opponent = paddles.create(750, 30, 'paddle');
+	ball = game.add.sprite(400, 300, 'ball');
 
 	game.physics.enable(player, Phaser.Physics.ARCADE);
 	game.physics.enable(opponent, Phaser.Physics.ARCADE);
+	game.physics.enable(ball, Phaser.Physics.ARCADE);
 
 	player.body.collideWorldBounds = true;
 	opponent.body.collideWorldBounds = true;
+	ball.body.collideWorldBounds = true;
+
+	ball.body.bounce.setTo(1,1);
+	player.body.bounce.setTo(1,1);
+	opponent.body.bounce.setTo(1,1);
 
 	game.input.keyboard.addKeyCapture( [ Phaser.Keyboard.W, Phaser.Keyboard.S, Phaser.Keyboard.SPACEBAR ] );
+	game.input.onDown.add(launch, this);
+}
+
+function launch() {
+	XVector = -(game.input.x - ball.x);
+	YVector = -(game.input.y - ball.y);
+	ball.body.velocity.setTo(XVector, YVector);
 }
 
 function update() {
-	
+
 	if( game.input.keyboard.isDown(Phaser.Keyboard.S) && player.body.velocity.y < 200 ){
 		player.body.velocity.y += 50;
 	}else if( game.input.keyboard.isDown(Phaser.Keyboard.W) && player.body.velocity.y > -200  ){
@@ -30,4 +49,7 @@ function update() {
 	}else{
 		player.body.velocity.y += 10;
 	}
+
+	game.physics.arcade.collide(paddles, ball);
+
 }
